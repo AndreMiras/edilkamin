a36c007546776bc66d5881f99fb976c676af830f
Andre Miras <andre.miras@gmail.com>
2022-02-22 11:16:56 +0100

    :lipstick: style: FontAwesome

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import { Container } from "react-bootstrap";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { fas } from "@fortawesome/free-solid-svg-icons";
 import Home from "./components/Home";
 import Header from "./components/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
+
+library.add(fas, fab);
 
 const App = (): JSX.Element => {
   return (
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,17 @@
-import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Container, Nav, Navbar } from "react-bootstrap";
 
 const Header = (): JSX.Element => (
   <Navbar bg="dark" variant="dark" expand="sm">
     <Container>
-      <Navbar.Brand href={process.env.PUBLIC_URL}>Edilkamin</Navbar.Brand>
+      <Navbar.Brand href={process.env.PUBLIC_URL}>
+        <FontAwesomeIcon icon={["fas", "fire"]} /> Edilkamin
+      </Navbar.Brand>
       <Navbar.Toggle />
       <Navbar.Collapse>
         <Nav className="mr-auto">
           <Nav.Link href="https://github.com/AndreMiras/edilkamin">
-            About
+            <FontAwesomeIcon icon={["fab", "github-alt"]} /> About
           </Nav.Link>
         </Nav>
       </Navbar.Collapse>
