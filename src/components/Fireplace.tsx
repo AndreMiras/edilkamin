be6a897442d7916b5c5078dfabb3a8f8d3fcd99c
Andre Miras <andre.miras@gmail.com>
2022-02-22 12:10:14 +0100

    :sparkles: feat: Fireplace info

--- src/components/Fireplace.tsx
+++ src/components/Fireplace.tsx
@@ -1,8 +1,33 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { Accordion } from "react-bootstrap";
+import { deviceInfo } from "edilkamin";
 
 const Fireplace = (): JSX.Element => {
   const { mac } = useParams<"mac">();
-  return <>Fireplace: {mac}</>;
+  // TODO: export type
+  const [info, setInfo] = useState<any | null>(null);
+
+  useEffect(() => {
+    const fetch = async () => {
+      mac && setInfo(await deviceInfo(mac));
+    };
+    fetch();
+  }, [mac]);
+
+  return (
+    <>
+      <h2>Fireplace: {mac}</h2>
+      <Accordion defaultActiveKey="0" className="mt-2">
+        <Accordion.Item eventKey="0">
+          <Accordion.Header>Debug</Accordion.Header>
+          <Accordion.Body>
+            <pre>{JSON.stringify(info, null, 2)}</pre>
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
+    </>
+  );
 };
 
 export default Fireplace;
